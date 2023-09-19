28af9d5a581dedd0c1a90db7ef44def5269e8c96
jschlunk <93251498+jschlunk@users.noreply.github.com>
2023-09-19 11:42:41 +0200

    more handler

--- src/main/java/com/example/demo/customer/CustomerControllerV2.java
+++ src/main/java/com/example/demo/customer/CustomerControllerV2.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.exception.ApiRequestException;
+
 import jakarta.validation.Valid;
 
 @RestController
@@ -34,6 +36,13 @@ public class CustomerControllerV2 {
     @GetMapping(path = "{customerId}")
     Customer getCustomer(@PathVariable("customerId") Long id) {
         return customerService.getCustomer(id);
+
+    }
+
+    @GetMapping(path = "{customerId}/exception")
+    Customer getCustomerException(@PathVariable("customerId") Long id) {
+        throw new ApiRequestException(
+                "ApiRequestException for customer" + id);
 
     }
 
--- src/main/java/com/example/demo/exception/ApiExceptionHandler.java
+++ src/main/java/com/example/demo/exception/ApiExceptionHandler.java
@@ -4,9 +4,11 @@ import java.time.ZonedDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
-public class ExceptionHandler {
+public class ApiExceptionHandler {
 
+    @ExceptionHandler(value = { ApiRequestException.class })
     public ResponseEntity<Object> handleApiRequestException(
             ApiRequestException e) {
         ApiException apiException = new ApiException(e.getMessage(), e, HttpStatus.BAD_REQUEST, ZonedDateTime.now());
