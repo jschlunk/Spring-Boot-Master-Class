8a513c9968b391fe93c3c4a6946b26e989bd86f9
jschlunk <93251498+jschlunk@users.noreply.github.com>
2023-09-19 16:50:25 +0200

    actuator

--- src/main/java/com/example/demo/exception/ApiExceptionHandler.java
+++ src/main/java/com/example/demo/exception/ApiExceptionHandler.java
@@ -2,15 +2,11 @@ package com.example.demo.exception;
 
 import java.time.ZonedDateTime;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 public class ApiExceptionHandler {
-
-    private final static Logger LOGGER = LoggerFactory.getLogger(ApiExceptionHandler.class);
 
     @ExceptionHandler(value = { ApiRequestException.class })
     public ResponseEntity<Object> handleApiRequestException(
